80b82bd413214c1ae3fa6f245181a0bdd2a77fbc
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2023-11-28 20:55:31 +0100

    fixed authenticatioin issue

--- src/app/[locale]/(admin_dashboard)/(routes)/users/page.tsx
+++ src/app/[locale]/(admin_dashboard)/(routes)/users/page.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
 import UserBody from "./body";
+import { getPermissions } from "@/lib/server";
+import { getServerSession } from "next-auth/next";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
 const page = async () => {
+  const session = await getServerSession(options);
+  const permissions = await getPermissions(session);
+
+  if (!permissions.length) {
+    return (
+      <main className="flex flex-col items-center p-5">
+        You are not authorized to view this page. Please login. <br/>
+        <Link href="/" className="bg-primary text-white py-2 px-3.5 my-3">
+          Go to login page
+        </Link>
+      </main>
+    );
+  }
+
   return (
     <div className="mx-5">
       <UserBody/>
