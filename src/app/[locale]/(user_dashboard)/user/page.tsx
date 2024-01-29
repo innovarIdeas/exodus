8e9403749c93272a6fa6c7f0781b99f3f9c1c3f0
ALPHAWGA BAMIDELE <alphawgas@gmail.com>
2024-01-29 08:00:00 +0100

    update

---
+++ src/app/[locale]/(user_dashboard)/user/page.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+export default function CustomerDashboard () {
+  return (
+    <div className="w-full h-fit bg-main">
+      <h1 className="text-[30px] text-center text-white pt-5">
+        Categories to explore
+      </h1>
+      <div className="w-[80%] mx-auto h-fit grid grid-cols-3 gap-[50px] mt-[30px]">
+Customer Dashboard
+      </div>
+    </div>
+  );
+}
