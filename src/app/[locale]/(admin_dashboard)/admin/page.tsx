abdda8089e9b8066c82bb85ad1231da2d0ae11c9
Purpose11 <adebayofatai924@gmail.com>
2024-02-13 11:58:55 +0100

    function check for admin role removed

--- src/app/[locale]/(admin_dashboard)/admin/page.tsx
+++ src/app/[locale]/(admin_dashboard)/admin/page.tsx
@@ -15,131 +15,130 @@ export default function AdminDashboard () {
     redirect("/login");
   }
 
-  if (ChecksUserPermission(PERMISSION_CODES.ADMIN)) {
-    return (
-      <main className="w-screen h-screen flex flex-col justify-start bg-[url('/background.svg')] bg-cover bg-repeat  md:bg-repeat">
-        <div className="text-lg font-semibold text-gray2 animate-[bounce_2s_ease-in-out] bg-transwhite shadow-lg rounded-lg p-5 m-2 w-[20%] md:w-[30%] sm:w-full">
+  return (
+    <main className="w-screen h-screen flex flex-col justify-start bg-[url('/background.svg')] bg-cover bg-repeat  md:bg-repeat">
+      <div className="text-lg font-semibold text-gray2 animate-[bounce_2s_ease-in-out] bg-transwhite shadow-lg rounded-lg p-5 m-2 w-[20%] md:w-[30%] sm:w-full">
           Welcome <span className="text-main font-bold">Jane Doe</span>
-        </div>
+      </div>
 
-        <div className="grid grid-cols-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 bg-transwhite gap-4   shadow-lg rounded-lg mx-[10%] px-[5%]">
-          {ChecksUserPermission(PERMISSION_CODES.CREATE_BOOK) ||
+      <div className="grid grid-cols-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 bg-transwhite gap-4   shadow-lg rounded-lg mx-[10%] px-[5%]">
+        {ChecksUserPermission(PERMISSION_CODES.CREATE_BOOK) ||
           ChecksUserPermission(PERMISSION_CODES.READ_BOOK) ||
           ChecksUserPermission(PERMISSION_CODES.UPDATE_BOOK) ||
           ChecksUserPermission(PERMISSION_CODES.DELETE_BOOK) ? (
-              <Link
-                className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
-                href="/books"
-              >
-                <Image src="/img/books.png" alt="Logo" width={64} height={64} />
-                <h3 className="mt-4 font-bold text-sm text-black">Books</h3>
-              </Link>
-            ) : (
-              ""
-            )}
-          {ChecksUserPermission(PERMISSION_CODES.CREATE_ORDER) ||
+            <Link
+              className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
+              href="/books"
+            >
+              <Image src="/img/books.png" alt="Logo" width={64} height={64} />
+              <h3 className="mt-4 font-bold text-sm text-black">Books</h3>
+            </Link>
+          ) : (
+            ""
+          )}
+        {ChecksUserPermission(PERMISSION_CODES.CREATE_ORDER) ||
           ChecksUserPermission(PERMISSION_CODES.READ_ORDER) ||
           ChecksUserPermission(PERMISSION_CODES.UPDATE_ORDER) ||
           ChecksUserPermission(PERMISSION_CODES.DELETE_ORDER) ? (
-              <Link
-                className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
-                href="/orders"
-              >
-                <Image src="/img/orders.png" alt="Logo" width={64} height={64} />
-                <h3 className="mt-4 font-bold text-sm text-black">Orders</h3>
-              </Link>
-            ) : (
-              ""
-            )}
-          {ChecksUserPermission(PERMISSION_CODES.CREATE_USER) ||
+            <Link
+              className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
+              href="/orders"
+            >
+              <Image src="/img/orders.png" alt="Logo" width={64} height={64} />
+              <h3 className="mt-4 font-bold text-sm text-black">Orders</h3>
+            </Link>
+          ) : (
+            ""
+          )}
+        {ChecksUserPermission(PERMISSION_CODES.CREATE_USER) ||
           ChecksUserPermission(PERMISSION_CODES.READ_USER) ||
           ChecksUserPermission(PERMISSION_CODES.UPDATE_USER) ||
           ChecksUserPermission(PERMISSION_CODES.DELETE_USER) ? (
-              <Link
-                className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
-                href="/users"
-              >
-                <Image src="/img/users.png" alt="Logo" width={64} height={64} />
-                <h3 className="mt-4 font-bold text-sm text-black">Users</h3>
-              </Link>
-            ) : (
-              ""
-            )}
-          {ChecksUserPermission(PERMISSION_CODES.CREATE_INVOICE) ||
+            <Link
+              className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
+              href="/users"
+            >
+              <Image src="/img/users.png" alt="Logo" width={64} height={64} />
+              <h3 className="mt-4 font-bold text-sm text-black">Users</h3>
+            </Link>
+          ) : (
+            ""
+          )}
+        {ChecksUserPermission(PERMISSION_CODES.CREATE_INVOICE) ||
           ChecksUserPermission(PERMISSION_CODES.READ_INVOICE) ||
           ChecksUserPermission(PERMISSION_CODES.UPDATE_INVOICE) ||
           ChecksUserPermission(PERMISSION_CODES.DELETE_INVOICE) ? (
-              <Link
-                className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
-                href="/invoices"
-              >
-                <Image src="/img/invoice.png" alt="Logo" width={64} height={64} />
-                <h3 className="mt-4 font-bold text-sm text-black">Invoices</h3>
-              </Link>
-            ) : (
-              ""
-            )}
-          {ChecksUserPermission(PERMISSION_CODES.CREATE_DISCOUNT) ||
+            <Link
+              className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
+              href="/invoices"
+            >
+              <Image src="/img/invoice.png" alt="Logo" width={64} height={64} />
+              <h3 className="mt-4 font-bold text-sm text-black">Invoices</h3>
+            </Link>
+          ) : (
+            ""
+          )}
+        {ChecksUserPermission(PERMISSION_CODES.CREATE_DISCOUNT) ||
           ChecksUserPermission(PERMISSION_CODES.READ_DISCOUNT) ||
           ChecksUserPermission(PERMISSION_CODES.UPDATE_DISCOUNT) ||
           ChecksUserPermission(PERMISSION_CODES.DELETE_DISCOUNT) ? (
-              <Link
-                className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
-                href="/discounts"
-              >
-                <Image
-                  src="/img/discounts.png"
-                  alt="Logo"
-                  width={64}
-                  height={64}
-                />
-                <h3 className="mt-4 font-bold text-sm text-black">Discounts</h3>
-              </Link>
-            ) : (
-              ""
-            )}
-          {ChecksUserPermission(PERMISSION_CODES.CREATE_TRANSACTION) ||
+            <Link
+              className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
+              href="/discounts"
+            >
+              <Image
+                src="/img/discounts.png"
+                alt="Logo"
+                width={64}
+                height={64}
+              />
+              <h3 className="mt-4 font-bold text-sm text-black">Discounts</h3>
+            </Link>
+          ) : (
+            ""
+          )}
+        {ChecksUserPermission(PERMISSION_CODES.CREATE_TRANSACTION) ||
           ChecksUserPermission(PERMISSION_CODES.READ_TRANSACTOIN) ||
           ChecksUserPermission(PERMISSION_CODES.UPDATE_TRANSACTION) ||
           ChecksUserPermission(PERMISSION_CODES.DELETE_TRANSACTION) ? (
-              <Link
-                className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
-                href="/transactions"
-              >
-                <Image
-                  src="/img/transaction.png"
-                  alt="Logo"
-                  width={64}
-                  height={64}
-                />
-                <h3 className="mt-4 font-bold text-sm text-black">
+            <Link
+              className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
+              href="/transactions"
+            >
+              <Image
+                src="/img/transaction.png"
+                alt="Logo"
+                width={64}
+                height={64}
+              />
+              <h3 className="mt-4 font-bold text-sm text-black">
                 Transactions
-                </h3>
-              </Link>
-            ) : (
-              ""
-            )}
-          {ChecksUserPermission(PERMISSION_CODES.CREATE_CONSTANT) ||
+              </h3>
+            </Link>
+          ) : (
+            ""
+          )}
+        {ChecksUserPermission(PERMISSION_CODES.CREATE_CONSTANT) ||
           ChecksUserPermission(PERMISSION_CODES.READ_CONSTANT) ||
           ChecksUserPermission(PERMISSION_CODES.UPDATE_CONSTANT) ||
           ChecksUserPermission(PERMISSION_CODES.DELETE_CONSTANT) ? (
-              <Link
-                className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
-                href="/constants"
-              >
-                <Image
-                  src="/img/constants.png"
-                  alt="Logo"
-                  width={64}
-                  height={64}
-                />
-                <h3 className="mt-4 font-bold text-sm text-black">Constants</h3>
-              </Link>
-            ) : (
-              ""
-            )}
-        </div>
-      </main>
-    );
-  }
+            <Link
+              className="py-8 px-10 text-center border border-white shadow-lg bg-white my-6 items-center flex flex-col rounded-3xl mx-4"
+              href="/constants"
+            >
+              <Image
+                src="/img/constants.png"
+                alt="Logo"
+                width={64}
+                height={64}
+              />
+              <h3 className="mt-4 font-bold text-sm text-black">Constants</h3>
+            </Link>
+          ) : (
+            ""
+          )}
+      </div>
+    </main>
+  );
 }
+
