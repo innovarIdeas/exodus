3c593b7566248a2e6d8548050769b42522d70f0a
Alpha <alphawgas@gmail.com>
2024-03-06 11:05:30 +0100

    update

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 import WelcomeEmail from "@emails";
 import { render } from "@react-email/render";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_B4wAhZiy_BEm2rCdR6RS9KXytaLTCtX3r");
 
 export async function POST (request: NextRequest) {
   try {
