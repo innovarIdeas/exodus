d75422dad0062762576c8b32492c1a21a965d6ab
elvissamuel <77748602+elvissamuel@users.noreply.github.com>
2024-04-12 15:27:38 +0100

    add updates

---
+++ src/components/OrderTemplateFormWorkInProgress.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OrderTemplateFormWorkInProgress = () => {
+  return (
+    <div>
+      <h1 className="text-center font-xl font-semibold">
+        Form is under development!
+      </h1>
+    </div>
+  );
+};
+
+export default OrderTemplateFormWorkInProgress;
