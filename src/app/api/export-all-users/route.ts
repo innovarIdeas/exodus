c14ef2a14726ea377a0378b9c0c89be7ddcde757
elvissamuel <77748602+elvissamuel@users.noreply.github.com>
2024-04-22 17:41:44 +0100

    update fix

--- src/app/api/export-all-users/route.ts
+++ src/app/api/export-all-users/route.ts
@@ -7,7 +7,7 @@ export async function GET () {
   const session = await getServerSession(options);
 
   try {
-    const books = await prisma.user.findMany({ where: { deleted_at: null } });
+    const books = await prisma.user.findMany({ where: { deleted_at: null }, select: { id: true, name: true, email: true, created_at: true, first_seen: true, last_seen: true  } });
 
     if (!session) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
